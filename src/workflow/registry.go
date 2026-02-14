89bd456378c09b850dc05892a93f29a39eb50646
Jason Liu <111265664+liug26@users.noreply.github.com>
2026-02-14 10:30:15 -0800

    Rename basic run to aegean in src/workflow/

--- src/workflow/registry.go
+++ src/workflow/registry.go
@@ -3,26 +3,26 @@ package workflow
 import (
 	"aegean/components/exec"
 	"aegean/nodes"
-	basicrunworkflow "aegean/workflow/basic_run"
+	aegeanworkflow "aegean/workflow/aegean"
 	reqraceworkflow "aegean/workflow/req_race"
 )
 
 var ClientWorkflows = map[string]func(c *nodes.Client){
-	"basic_run_client":    basicrunworkflow.ClientRequestLogic,
-	"basic_run_pipelined": basicrunworkflow.ClientRequestLogicPipelined,
-	"req_race_client":     reqraceworkflow.ClientRequestLogic,
+	"aegean_client":    aegeanworkflow.ClientRequestLogic,
+	"aegean_pipelined": aegeanworkflow.ClientRequestLogicPipelined,
+	"req_race_client":  reqraceworkflow.ClientRequestLogic,
 }
 
 var ExecWorkflows = map[string]exec.ExecuteRequestFunc{
-	"basic_run_backend":           basicrunworkflow.ExecuteRequestBackend,
-	"basic_run_backend_diverge_1": basicrunworkflow.ExecuteRequestBackendDivergeOneNode,
-	"basic_run_backend_diverge_2": basicrunworkflow.ExecuteRequestBackendDivergeTwoNode,
-	"basic_run_backend_diverge_3": basicrunworkflow.ExecuteRequestBackendDivergeThreeNode,
-	"basic_run_middle":            basicrunworkflow.ExecuteRequestMiddle,
-	"basic_run_middle_diverge_1":  basicrunworkflow.ExecuteRequestMiddleDivergeOneNode,
-	"basic_run_middle_diverge_2":  basicrunworkflow.ExecuteRequestMiddleDivergeTwoNode,
-	"basic_run_middle_diverge_3":  basicrunworkflow.ExecuteRequestMiddleDivergeThreeNode,
-	"req_race_middle":             reqraceworkflow.ExecuteRequestMiddle,
-	"req_race_backend_1":          reqraceworkflow.ExecuteRequestBackend1,
-	"req_race_backend_2":          reqraceworkflow.ExecuteRequestBackend2,
+	"aegean_backend":           aegeanworkflow.ExecuteRequestBackend,
+	"aegean_backend_diverge_1": aegeanworkflow.ExecuteRequestBackendDivergeOneNode,
+	"aegean_backend_diverge_2": aegeanworkflow.ExecuteRequestBackendDivergeTwoNode,
+	"aegean_backend_diverge_3": aegeanworkflow.ExecuteRequestBackendDivergeThreeNode,
+	"aegean_middle":            aegeanworkflow.ExecuteRequestMiddle,
+	"aegean_middle_diverge_1":  aegeanworkflow.ExecuteRequestMiddleDivergeOneNode,
+	"aegean_middle_diverge_2":  aegeanworkflow.ExecuteRequestMiddleDivergeTwoNode,
+	"aegean_middle_diverge_3":  aegeanworkflow.ExecuteRequestMiddleDivergeThreeNode,
+	"req_race_middle":          reqraceworkflow.ExecuteRequestMiddle,
+	"req_race_backend_1":       reqraceworkflow.ExecuteRequestBackend1,
+	"req_race_backend_2":       reqraceworkflow.ExecuteRequestBackend2,
 }
