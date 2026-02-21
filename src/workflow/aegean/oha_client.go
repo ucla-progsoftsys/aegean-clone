47d4e62769f40ccd524a8b8ab59b6f6fe9ff9ec8
Jason Liu <111265664+liug26@users.noreply.github.com>
2026-02-20 19:32:52 -0800

    Add placeholder oha client workflow for Aegean

--- src/workflow/aegean/oha_client.go
+++ src/workflow/aegean/oha_client.go
@@ -1,7 +1,35 @@
 package aegeanworkflow
 
-import "aegean/nodes"
+import (
+	"aegean/nodes"
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+)
+
+const (
+	ohaTargetURL = "http://node2:8000/"
+	ohaRequestN  = 1000
+)
 
 func OhaClientRequestLogic(c *nodes.Client) {
 	_ = c
+
+	command := fmt.Sprintf(
+		`oha -n %d -m POST -H "Content-Type: application/json" -d '{"oha_client":true}' %s`,
+		ohaRequestN,
+		ohaTargetURL,
+	)
+	cmd := exec.Command(
+		"bash",
+		"-lc",
+		command,
+	)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	if err := cmd.Run(); err != nil {
+		log.Printf("oha client request logic failed: %v", err)
+	}
 }
